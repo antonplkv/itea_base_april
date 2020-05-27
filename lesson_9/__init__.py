29b173c253906f574a3965c856ecf604ec903f68
Anton <polyakov.anton@ukr.net>
2020-05-27 23:11:01 +0300

    lesson 10

---
+++ lesson_9/__init__.py
@@ -0,0 +1,26 @@
+def a(arg4, arg5):
+    pass
+
+
+def b(arg1, arg2, arg3):
+    pass
+
+
+a_list = [
+    (1, 2, 3, 4, 5, 6, 7, 8,),
+    (1, 2, 3, 4, 5, 6, 7, 8),
+    (1, 2, 3, 4, 5, 6, 7, 8),
+]
+
+
+
+b = {1: 4, 2: 8}
+
+a(arg5=20, arg4=30)
+
+
+products = []
+
+
+class LightCar:
+    pass
